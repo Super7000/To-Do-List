32a9a9ed5828b9a39b677b0ec5ce13a7527d8c16
Subrata Chowdhury <subratachowdhury7000@gmail.com>
2024-07-09 12:13:21 +0530

    Login page design complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import LogInPage from './components/LogInPage'
+import ToDosPage from './components/HomePage'
 
 function App() {
-  return (
-    <>
-    </>
-  )
+	return (
+		<div className='container-fluid d-flex justify-content-center align-items-center' style={{height: '100vh'}}>
+			{/* <LogInPage /> */}
+			<ToDosPage />
+		</div>
+	)
 }
 
 export default App
